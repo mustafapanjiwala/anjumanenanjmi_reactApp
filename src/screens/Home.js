27d9568a7b15bf5f695edec6812dcf5a56df1b4d
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2021-08-10 10:42:55 +0530

    second day

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -34,7 +34,7 @@ const Home = () => {
         <div className="container full-height">
             <div className="row align-items-center justify-content-center full-height col-md-10 col-12 mx-auto my-auto">
                 <div className="youtube">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/DYqZKQpJF8Y" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfFullScreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/5lWbpBmFPlI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfFullScreen></iframe>
                 </div>
             </div>
         </div>
