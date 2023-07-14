06d6d7a83b8618b5a815ede19fa87b3fe72ec0cd
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2023-07-14 13:14:43 +0530

    indentation

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,63 +1,79 @@
-import React from 'react'
-import "./Login"
+import React from "react";
+import "./Login";
 
 const Home = () => {
-    return (
-        <>
-          <nav className="navbar navbar-custom navbar-expand-lg navbar-light">
+  return (
+    <>
+      <nav className="navbar navbar-custom navbar-expand-lg navbar-light">
         <div className="container-fluid ">
-            <div className="flex2 d-flex justify-content-between align-items-end w-100">
-                <div className="col-lg-6 col-md-6 col-12">
-                    <a className=" navbar-brand d-inline-flex align-items-center" href="Login"
-                        style={{marginLeft:"10px"}}>
-                        <img src="/img/logo.png" alt="logo" />
-                        <div className="flex">
-                            <h1>Anjuman-e-Najmi</h1>
-                            <h5 className="heading" style={{float: "right"}}>Hakimi Mohalla Ujjain</h5>
-                        </div>
-                    </a>
+          <div className="flex2 d-flex justify-content-between align-items-end w-100">
+            <div className="col-lg-6 col-md-6 col-12">
+              <a
+                className=" navbar-brand d-inline-flex align-items-center"
+                href="Login"
+                style={{ marginLeft: "10px" }}
+              >
+                <img src="/img/logo.png" alt="logo" />
+                <div className="flex">
+                  <h1>Anjuman-e-Najmi</h1>
+                  <h5 className="heading" style={{ float: "right" }}>
+                    Hakimi Mohalla Ujjain
+                  </h5>
                 </div>
-                <a href="/">
-                    <div className="logout">
-                        <button  className="btn btn-warning btn-lg" type="submit" value="Logout" name="submit" > Logout </button>
-                    </div>
-                </a>
-
-
+              </a>
             </div>
-
-
+            <a href="/">
+              <div className="logout">
+                <button
+                  className="btn btn-warning btn-lg"
+                  type="submit"
+                  value="Logout"
+                  name="submit"
+                >
+                  {" "}
+                  Logout{" "}
+                </button>
+              </div>
+            </a>
+          </div>
         </div>
-    </nav>
+      </nav>
 
-    <section id="hero">
+      <section id="hero">
         <div className="container full-height">
-            <div className="row align-items-center justify-content-center full-height col-md-10 col-12 mx-auto my-auto">
-                <div className="youtube">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/IJk-sZF65no" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    {/* <h1 style={{fontSize: "3rem"}}>No Current Events </h1>   */}
-                </div>
+          <div className="row align-items-center justify-content-center full-height col-md-10 col-12 mx-auto my-auto">
+            <div className="youtube">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/nk0qACYkbQM"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+              {/* <h1 style={{fontSize: "3rem"}}>No Current Events </h1>   */}
             </div>
+          </div>
         </div>
-    </section>
+      </section>
 
-
-    <section className="footer">
+      <section className="footer">
         <img src="/img/Footer%20panel.png" alt="" />
         <div className="container">
-
-            <div className="row text-center col-md-12 col-12">
-
-                <p>
-                    © 2021 Anjuman-E-Najmi - All rights reserved.<br />
-                    <span>Designed and developed by<b> Mustafa Panjiwala</b>.</span>
-                </p>
-            </div>
+          <div className="row text-center col-md-12 col-12">
+            <p>
+              © 2021 Anjuman-E-Najmi - All rights reserved.
+              <br />
+              <span>
+                Designed and developed by<b> Mustafa Panjiwala</b>.
+              </span>
+            </p>
+          </div>
         </div>
-    </section>
-   
-        </>
-    )
-}
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,88 +1,92 @@
-import React, { useState } from 'react'
-import data from "../loginData.json"
-import Home from "./Home"
+import React, { useState } from "react";
+import data from "../loginData.json";
+import Home from "./Home";
 
-const Login = () => { 
+const Login = () => {
+  const [its, setIts] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const [its, setIts] = useState("")
-    const [password, setPassword] = useState("" )
-    const [error, setError] = useState(false)
-    const [loggedIn, setLoggedIn] = useState(false)
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!data[its] || data[its].password !== password){
-
-            console.log("incorrect");
-            setError(true);
-
-        }
-        else{
-            setLoggedIn(true);
-            console.log("correct");
-        }    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!data[its] || data[its].password !== password) {
+      console.log("incorrect");
+      setError(true);
+    } else {
+      setLoggedIn(true);
+      console.log("correct");
     }
+  };
 
-    if(loggedIn)
-        return <Home/>
-    // console.log(error)
-    return (
-
-        <>
-    <nav className="navbar navbar-custom navbar-expand-lg navbar-light">
+  if (loggedIn) return <Home />;
+  return (
+    <>
+      <nav className="navbar navbar-custom navbar-expand-lg navbar-light">
         <div className="container-fluid ">
-            <a className=" navbar-brand d-inline-flex align-items-center" href="index.php" stle={{marginLeft: "10px"}} >
-                <img src="/img/logo.png" alt="logo" />
-                <div className="flex">
-                    <h1>Anjuman-e-Najmi</h1>
-                    <h5 className="heading" style={{float: "right"}}>Hakimi Mohalla Ujjain</h5>
-                </div>
-            </a>
-
+          <a
+            className=" navbar-brand d-inline-flex align-items-center"
+            href="index.php"
+            stle={{ marginLeft: "10px" }}
+          >
+            <img src="/img/logo.png" alt="logo" />
+            <div className="flex">
+              <h1>Anjuman-e-Najmi</h1>
+              <h5 className="heading" style={{ float: "right" }}>
+                Hakimi Mohalla Ujjain
+              </h5>
+            </div>
+          </a>
         </div>
-    </nav>
+      </nav>
 
-    <section id="hero">
+      <section id="hero">
         <img src="/img/jali.png" className="jali" alt="jali" />
         <div className="container full-height">
-            <div
-                className="row align-items-center justify-content-center full-height col-md-6 col-12 mx-auto my-auto logbox">
-                <div className="loginbox">
-                    <h1>login for majlis relay</h1>
-                    {error && 
-                    <p className='error'>Incorrect ITS id or Password</p>
-                    }
-                    <form onSubmit={handleSubmit} method="POST">
-                        <input type="text" placeholder="Enter ITS" name="its" value={its} onChange={(e) => setIts(e.target.value)}  />
+          <div className="row align-items-center justify-content-center full-height col-md-6 col-12 mx-auto my-auto logbox">
+            <div className="loginbox">
+              <h1>login for majlis relay</h1>
+              {error && <p className="error">Incorrect ITS id or Password</p>}
+              <form onSubmit={handleSubmit} method="POST">
+                <input
+                  type="text"
+                  placeholder="Enter ITS"
+                  name="its"
+                  value={its}
+                  onChange={(e) => setIts(e.target.value)}
+                />
 
-                        <input type="password" placeholder="Enter Password" name="password" value={password} onChange = {(e) => setPassword(e.target.value)} />
-                        <br></br>
-                        <input type="submit" value="Login" name="submit" />
-                        <br></br>
-                    </form>
-                </div>
-
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <br></br>
+                <input type="submit" value="Login" name="submit" />
+                <br></br>
+              </form>
             </div>
+          </div>
         </div>
-    </section>
+      </section>
 
-
-    <section className="footer">
+      <section className="footer">
         <img src="/img/Footer%20panel.png" alt="" />
         <div className="container">
-
-            <div className="row text-center col-md-12 col-12">
-
-                <p>
-                    © 2021 Anjuman-E-Najmi - All rights reserved.<br></br>
-                    <span>Designed and developed by<b> Mustafa Panjiwala</b>.</span>
-                </p>
-            </div>
+          <div className="row text-center col-md-12 col-12">
+            <p>
+              © 2021 Anjuman-E-Najmi - All rights reserved.<br></br>
+              <span>
+                Designed and developed by<b> Mustafa Panjiwala</b>.
+              </span>
+            </p>
+          </div>
         </div>
-    </section>
-   
-        </>
-    )
-}
+      </section>
+    </>
+  );
+};
 
-export default Login
+export default Login;
